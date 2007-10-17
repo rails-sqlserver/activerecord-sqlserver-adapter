f06a3670b43cd96b12381628e1dc5539ec54cc04
lawrence <lawrence@5ecf4fe2-1ee6-0310-87b1-e25e094e27de>
2007-10-17 00:06:31 +0000

    Cleaning up more double code
    git-svn-id: http://dev.rubyonrails.org/svn/rails/adapters/sqlserver@7952 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- test/connections/native_sqlserver_odbc/connection.rb
+++ test/connections/native_sqlserver_odbc/connection.rb
@@ -23,28 +23,3 @@ ActiveRecord::Base.configurations = {
 
 ActiveRecord::Base.establish_connection 'arunit'
 Course.establish_connection 'arunit2'
-print "Using native SQLServer via ODBC\n"
-require_dependency 'fixtures/course'
-require 'logger'
-
-ActiveRecord::Base.logger = Logger.new("debug.log")
-
-ActiveRecord::Base.configurations = {
-  'arunit' => {
-    :adapter  => 'sqlserver',
-    :mode     => 'ODBC',
-    :host     => 'localhost',
-    :username => 'rails',
-    :dsn => 'activerecord_unittest'
-  },
-  'arunit2' => {
-    :adapter  => 'sqlserver',
-    :mode     => 'ODBC',
-    :host     => 'localhost',
-    :username => 'rails',
-    :dsn => 'activerecord_unittest2'
-  }
-}
-
-ActiveRecord::Base.establish_connection 'arunit'
-Course.establish_connection 'arunit2'
