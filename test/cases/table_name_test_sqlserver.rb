38aa198cc274b93fcb5840cc10a5bd0ed515fcef
Murray Steele <muz@h-lame.com>
2008-07-22 19:17:14 +0100

    Fixing table_name_test_sqlserver.rb to not leave it's droppings all over the Order model (e.g. repair the table name with what it used to be)
    Should we create / use a repair_table_name macro for this?

--- test/cases/table_name_test_sqlserver.rb
+++ test/cases/table_name_test_sqlserver.rb
@@ -1,23 +1,27 @@
 require "cases/helper"
 require 'active_record/schema'
+require 'models/order'
 
-if ActiveRecord::Base.connection.supports_migrations?
-  class Order < ActiveRecord::Base
-    self.table_name = '[orders]'
+class TableNameTest < ActiveRecord::TestCase
+  self.use_transactional_fixtures = false
+
+  setup do
+    @old_name = Order.table_name
+    Order.table_name = '[orders]'
+  end
+  
+  teardown do
+    Order.table_name = @old_name
   end
 
-  class TableNameTest < ActiveRecord::TestCase
-    self.use_transactional_fixtures = false
-
-    # Ensures Model.columns works when using SQLServer escape characters.
-    # Enables legacy schemas using SQL reserved words as table names.
-    # Should work with table names with spaces as well ('table name').
-    def test_escaped_table_name
-      assert_nothing_raised do
-        ActiveRecord::Base.connection.select_all 'SELECT * FROM [orders]'
-      end
-      assert_equal '[orders]', Order.table_name
-      assert_equal 4, Order.columns.length
+  # Ensures Model.columns works when using SQLServer escape characters.
+  # Enables legacy schemas using SQL reserved words as table names.
+  # Should work with table names with spaces as well ('table name').
+  def test_escaped_table_name
+    assert_nothing_raised do
+      ActiveRecord::Base.connection.select_all 'SELECT * FROM [orders]'
     end
+    assert_equal '[orders]', Order.table_name
+    assert_equal 4, Order.columns.length
   end
 end
