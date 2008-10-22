4a08f1ba50a1f949e030af49c20e7ce6cc2c302b
Murray Steele <muz@h-lame.com>
2008-10-22 18:26:40 +0100

    Change test/cases/offset_and_limit_test_sqlserver.rb so that it actually runs properly.
    NOTE: this was a 2-stage commit because renaming and changing in one commit may mean that git will lose history.  The rename commit was 197c598a3e7a6538bcb4ee22d190a695ec7c7a54.

--- test/cases/offset_and_limit_test_sqlserver.rb
+++ test/cases/offset_and_limit_test_sqlserver.rb
@@ -1,4 +1,5 @@
 require 'cases/sqlserver_helper'
+require 'models/book'
 
 class WhenSelectingWithLimitOffsetAndLimitTest < ActiveRecord::TestCase
   def setup
@@ -31,13 +32,14 @@ class WhenSelectingWithLimitAndOffsetOffsetAndLimitTest < ActiveRecord::TestCase
   def setup
     @connection = ActiveRecord::Base.connection
     # we have to use a real table as we need the counts
-    @select_sql = 'SELECT * FROM accounts'
-    class Account < ActiveRecord::Base; end
-
-    # create 10 Accounts
-    (1..10).each {|i| Account.create!}
+    @select_sql = 'SELECT * FROM books'
+    @books = (1..10).map {|i| Book.create!}
   end
-
+  
+  def teardown
+    @books.each {|b| b.destroy}
+  end
+  
   def test_should_have_limit_if_offset_is_passed
     options = { :offset => 1 }
     assert_raise(ArgumentError) { @connection.add_limit_offset!(@select_sql, options) }
@@ -55,7 +57,7 @@ class WhenSelectingWithLimitAndOffsetOffsetAndLimitTest < ActiveRecord::TestCase
 
   def test_should_alter_SQL_to_limit_number_of_records_returned_offset_by_specified_amount
     options = { :limit => 3, :offset => 5 }
-    expected_sql = %&SELECT * FROM (SELECT TOP 3 * FROM (SELECT TOP 8 * FROM accounts) AS tmp1) AS tmp2&
+    expected_sql = %&SELECT * FROM (SELECT TOP 3 * FROM (SELECT TOP 8 * FROM books) AS tmp1) AS tmp2&
     assert_equal(expected_sql, @connection.add_limit_offset!(@select_sql, options))
   end
 
