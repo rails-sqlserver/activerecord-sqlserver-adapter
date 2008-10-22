f4be9684b5bb135e53dc75c5e143953836951441
Murray Steele <muz@h-lame.com>
2008-10-22 13:24:10 +0100

    Tests for the previous commit.
    The tests from http://github.com/adzap/sqlserver_adapter_mods/ are rspec, so rather than introduce another test dependency for the adapter, I stole a test from AR that was only being run for the MySQL adapter to test the same thing.

--- test/cases/adapter_test_sqlserver.rb
+++ test/cases/adapter_test_sqlserver.rb
@@ -129,19 +129,30 @@ end
 
 # NOTE: The existing schema_dumper_test doesn't test the limits of <4 limit things
 # for adapaters that aren't mysql, sqlite or postgres.  We should.
+# It also only tests non-standard id dumping for mysql.  We'll do that too.
 class SchemaDumperForSqlServerTest < ActiveRecord::TestCase
-  def test_schema_dump_includes_limit_constraint_for_integer_columns
+  def standard_dump(ignore_tables = [])
     stream = StringIO.new
-
-    ActiveRecord::SchemaDumper.ignore_tables = [/^(?!integer_limits)/]
+    ActiveRecord::SchemaDumper.ignore_tables = [*ignore_tables]
     ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
-    output = stream.string
+    stream.string
+  end
+  
+  def test_schema_dump_includes_limit_constraint_for_integer_columns
+    output = standard_dump(/^(?!integer_limits)/)
     assert_match %r{c_int_1.*:limit => 2}, output
     assert_match %r{c_int_2.*:limit => 2}, output
     assert_match %r{c_int_3.*}, output
     assert_match %r{c_int_4.*}, output
     assert_no_match %r{c_int_3.*:limit}, output
     assert_no_match %r{c_int_4.*:limit}, output
+  end
+  
+  def test_mysql_schema_dump_should_honor_nonstandard_primary_keys
+    output = standard_dump
+    match = output.match(%r{create_table "movies"(.*)do})
+    assert_not_nil(match, "nonstandardpk table not found")
+    assert_match %r(:primary_key => "movieid"), match[1], "non-standard primary key not preserved"
   end
 end
 
