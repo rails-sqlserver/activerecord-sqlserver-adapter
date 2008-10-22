91af9c1dcd6dec03d2869987cdd503cdb8133a0b
Ken Collins <ken@metaskills.net>
2008-10-22 12:37:05 -0400

    Better way to silence all migrations via test to stdout.

--- test/cases/aaaa_create_tables_test_sqlserver.rb
+++ test/cases/aaaa_create_tables_test_sqlserver.rb
@@ -5,7 +5,7 @@ class AAAACreateTablesTestSqlserver < ActiveRecord::TestCase
 
   def test_load_sqlserver_specific_schema
     sqlserver_specific_schema_file = File.expand_path(File.join(File.dirname(__FILE__), '..', 'schema', 'sqlserver_specific_schema.rb'))
-    ActiveRecord::Base.silence { eval(File.read(sqlserver_specific_schema_file)) }
+    eval(File.read(sqlserver_specific_schema_file))
     assert true
   end
   
