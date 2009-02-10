c916956df95f945a49d3f74ec4e68814fe8c9bd9
Ken Collins <ken@metaskills.net>
2009-02-09 20:33:13 -0500

    Rails2.3 - Coerced tests that ensure hash conditions on referenced tables are considered when eager loading with limit/offset. Information on these changes and the ticket in rails are.

--- test/cases/eager_association_test_sqlserver.rb
+++ test/cases/eager_association_test_sqlserver.rb
@@ -8,7 +8,11 @@ end
 
 class EagerAssociationTest < ActiveRecord::TestCase
   
-  COERCED_TESTS = [:test_count_with_include]
+  COERCED_TESTS = [
+    :test_count_with_include,
+    :test_eager_with_has_many_and_limit_and_high_offset_and_multiple_array_conditions,
+    :test_eager_with_has_many_and_limit_and_high_offset_and_multiple_hash_conditions
+  ]
   
   include SqlserverCoercedTest
   
@@ -16,6 +20,22 @@ class EagerAssociationTest < ActiveRecord::TestCase
   
   def test_coerced_test_count_with_include
     assert_equal 3, authors(:david).posts_with_comments.count(:conditions => "len(comments.body) > 15")
+  end
+  
+  def test_coerced_eager_with_has_many_and_limit_and_high_offset_and_multiple_array_conditions
+    assert_queries(2) do
+      posts = Post.find(:all, :include => [ :author, :comments ], :limit => 2, :offset => 10,
+        :conditions => [ "authors.name = ? and comments.body = ?", 'David', 'go crazy' ])
+      assert_equal 0, posts.size
+    end
+  end
+
+  def test_coerced_eager_with_has_many_and_limit_and_high_offset_and_multiple_hash_conditions
+    assert_queries(2) do
+      posts = Post.find(:all, :include => [ :author, :comments ], :limit => 2, :offset => 10,
+        :conditions => { 'authors.name' => 'David', 'comments.body' => 'go crazy' })
+      assert_equal 0, posts.size
+    end
   end
   
   
