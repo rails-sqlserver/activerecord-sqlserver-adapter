36a1809e85cf9ddc37c0927b9322710f2a8461c5
Ken Collins <ken@metaskills.net>
2009-09-10 10:59:07 -0400

    Release version 2.2.21.

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -183,7 +183,7 @@ module ActiveRecord
     class SQLServerAdapter < AbstractAdapter
       
       ADAPTER_NAME            = 'SQLServer'.freeze
-      VERSION                 = '2.2.20'.freeze
+      VERSION                 = '2.2.21'.freeze
       DATABASE_VERSION_REGEXP = /Microsoft SQL Server\s+(\d{4})/
       SUPPORTED_VERSIONS      = [2000,2005,2008].freeze
       LIMITABLE_TYPES         = ['string','integer','float','char','nchar','varchar','nvarchar'].freeze
