6d693a06a701b5aee2f865e73a34c8447c4847a6
Ken Collins <ken@metaskills.net>
2010-04-27 15:03:55 -0400

    Better IronRuby docs, release 2.3.5.

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -163,7 +163,7 @@ module ActiveRecord
     class SQLServerAdapter < AbstractAdapter
       
       ADAPTER_NAME                = 'SQLServer'.freeze
-      VERSION                     = '2.3.4'.freeze
+      VERSION                     = '2.3.5'.freeze
       DATABASE_VERSION_REGEXP     = /Microsoft SQL Server\s+(\d{4})/
       SUPPORTED_VERSIONS          = [2000,2005,2008].freeze
       LIMITABLE_TYPES             = ['string','integer','float','char','nchar','varchar','nvarchar'].freeze
