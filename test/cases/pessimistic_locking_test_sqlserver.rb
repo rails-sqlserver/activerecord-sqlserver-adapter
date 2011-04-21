c553de53b84335182c1e364d1375c81e8fa7ff77
Ken Collins <ken@metaskills.net>
2011-04-20 21:15:53 -0400

    [Rails31] Getting the tests to run with out deprecations for inheritable attributes and new IGNORED_SQL placement.

--- test/cases/pessimistic_locking_test_sqlserver.rb
+++ test/cases/pessimistic_locking_test_sqlserver.rb
@@ -75,49 +75,4 @@ class PessimisticLockingTestSqlserver < ActiveRecord::TestCase
   end
   
   
-  context 'For dueling concurrent connections' do
-    
-    use_concurrent_connections
-
-    should 'no locks does not wait' do
-      first, second = duel { Person.find 1 }
-      assert first.end > second.end
-    end
-    
-    should 'that second lock waits' do
-      assert [0.2, 1, 5].any? { |zzz|
-        first, second = duel(zzz) { Person.find 1, :lock => true }
-        second.end > first.end
-      }
-    end unless ruby_19?
-    
-  end
-  
-    
-  protected
-  
-  def duel(zzz = 5)
-    t0, t1, t2, t3 = nil, nil, nil, nil
-    a = Thread.new do
-      t0 = Time.now
-      Person.transaction do
-        yield
-        sleep zzz       # block thread 2 for zzz seconds
-      end
-      t1 = Time.now
-    end
-    b = Thread.new do
-      sleep zzz / 2.0   # ensure thread 1 tx starts first
-      t2 = Time.now
-      Person.transaction { yield }
-      t3 = Time.now
-    end
-    a.join
-    b.join
-    assert t1 > t0 + zzz
-    assert t2 > t0
-    assert t3 > t2
-    [t0.to_f..t1.to_f, t2.to_f..t3.to_f]
-  end
-  
 end
