9329a3a3b1a928b0a1ad97598e915001bf7c7322
raydog153 <ray.boutotte@gmail.com>
2013-06-26 08:31:34 -0400

    Fixing generating profile report to create output dir if needed, and code change for printing report

--- test/profile/helper.rb
+++ test/profile/helper.rb
@@ -2,17 +2,17 @@ require 'cases/sqlserver_helper'
 require 'ruby-prof'
 
 class ActiveRecord::TestCase
-  
-  
+
+
   protected
-  
+
   def ruby_profile(name)
     result = RubyProf.profile { yield }
     [:flat,:graph,:html].each do |printer|
       save_ruby_prof_report(result, name, printer)
     end
   end
-  
+
   def save_ruby_prof_report(result, name, printer)
     ptr = case printer
           when :flat  then RubyProf::FlatPrinter
@@ -21,9 +21,10 @@ class ActiveRecord::TestCase
           end
     file_name = printer == :html ? "#{name}_graph.html" : "#{name}_#{printer}.txt"
     file_path = File.join(SQLSERVER_TEST_ROOT, 'profile', 'output', file_name)
+    Dir.mkdir(File.join(SQLSERVER_TEST_ROOT, 'profile', 'output')) unless File.exists?(File.join(SQLSERVER_TEST_ROOT, 'profile', 'output'))
     File.open(file_path,'w') do |file|
-      printer == :html ? ptr.new(result).print(file) : ptr.new(result).print(file,0)
+      printer == :html ? ptr.new(result).print(file) : ptr.new(result).print(file,{})
     end
   end
-  
+
 end
