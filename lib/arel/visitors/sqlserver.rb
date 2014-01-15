4501c15feebdacf4604984828e551158f92d5b92
Anna <acarey@discoversdc.com>
2014-01-14 22:58:17 -0500

    Arel::Nodes::Ordering is now comparison friendly

--- lib/arel/visitors/sqlserver.rb
+++ lib/arel/visitors/sqlserver.rb
@@ -1,25 +1,7 @@
 require 'arel'
 
 module Arel
-
-  module Nodes
-
-    # Extending the Ordering class to be comparison friendly which allows us to call #uniq on a
-    # collection of them. See SelectManager#order for more details.
-    class Ordering < Arel::Nodes::Unary
-      def hash
-        expr.hash
-      end
-      def ==(other)
-        other.is_a?(Arel::Nodes::Ordering) && self.expr == other.expr
-      end
-      def eql?(other)
-        self == other
-      end
-    end
-
-  end
-
+  
   class SelectManager < Arel::TreeManager
     
     AR_CA_SQLSA_NAME = 'ActiveRecord::ConnectionAdapters::SQLServerAdapter'.freeze
