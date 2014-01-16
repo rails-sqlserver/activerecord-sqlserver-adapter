739102bf107f61c0fe311fa220fe6d735c1adec6
Anna <acarey@discoversdc.com>
2014-01-15 23:25:02 -0500

    Conditinally coerce tests that are new in rails 4.0.1

--- test/cases/disconnected_test_sqlserver.rb
+++ test/cases/disconnected_test_sqlserver.rb
@@ -1,32 +1,34 @@
-require 'cases/sqlserver_helper'
-require 'cases/disconnected_test'
+if Pathname.new('cases/disconnected_test').exist?
+  require 'cases/sqlserver_helper'
+  require 'cases/disconnected_test'
 
-class TestDisconnectedAdapter < ActiveRecord::TestCase
-  def setup
-    skip "TestDisconnectedAdapterSqlserver instead "
-  end
-end
-
-class TestDisconnectedAdapterSqlserver < ActiveRecord::TestCase
-  self.use_transactional_fixtures = false
-
-  def setup
-    skip "in-memory database mustn't disconnect" if in_memory_db?
-    @connection = ActiveRecord::Base.connection
+  class TestDisconnectedAdapter < ActiveRecord::TestCase
+    def setup
+      skip "TestDisconnectedAdapterSqlserver instead "
+    end
   end
 
-  def teardown
-    return if in_memory_db?
-    spec = ActiveRecord::Base.connection_config
-    ActiveRecord::Base.establish_connection(spec)
-  end
+  class TestDisconnectedAdapterSqlserver < ActiveRecord::TestCase
+    self.use_transactional_fixtures = false
 
-  test "can't execute statements while disconnected" do
-    with_auto_connect(false) do
-      @connection.execute "SELECT count(*) from products"
-      @connection.disconnect!
-      assert_raises(ActiveRecord::LostConnection) do
+    def setup
+      skip "in-memory database mustn't disconnect" if in_memory_db?
+      @connection = ActiveRecord::Base.connection
+    end
+
+    def teardown
+      return if in_memory_db?
+      spec = ActiveRecord::Base.connection_config
+      ActiveRecord::Base.establish_connection(spec)
+    end
+
+    test "can't execute statements while disconnected" do
+      with_auto_connect(false) do
         @connection.execute "SELECT count(*) from products"
+        @connection.disconnect!
+        assert_raises(ActiveRecord::LostConnection) do
+          @connection.execute "SELECT count(*) from products"
+        end
       end
     end
   end
--- test/cases/invalid_connection_test_sqlserver.rb
+++ test/cases/invalid_connection_test_sqlserver.rb
@@ -1,10 +1,13 @@
-require 'cases/sqlserver_helper'
-require 'cases/invalid_connection_test'
+#cases/invalid_connection_test was added in rails 4.0.1
+if Pathname.new('cases/invalid_connection_test').exist?
+  require 'cases/sqlserver_helper'
+  require 'cases/invalid_connection_test'
 
-class TestAdapterWithInvalidConnection < ActiveRecord::TestCase  
-   def setup
-    #The activerecord test arbitrarily used mysql (needed to use somthing that wasn't sqlite).
-    #It makes much more sense for use to use sqlserver
-    Bird.establish_connection adapter: 'sqlserver', database: 'i_do_not_exist'
+  class TestAdapterWithInvalidConnection < ActiveRecord::TestCase  
+     def setup
+      #The activerecord test arbitrarily used mysql (needed to use somthing that wasn't sqlite).
+      #It makes much more sense for us to use sqlserver
+      Bird.establish_connection adapter: 'sqlserver', database: 'i_do_not_exist'
+    end
   end
 end
