376d0364bdef59c39fefcb997c41ceaae3c47064
Anna Carey <acarey@discoverdsc.com>
2014-01-22 09:11:15 -0500

    arel now supports comparisons, but we still need to override the eql? method for Ordering

--- lib/arel/nodes_sqlserver.rb
+++ lib/arel/nodes_sqlserver.rb
@@ -1,19 +1,14 @@
 module Arel
   module Nodes
-
     # Extending the Ordering class to be comparison friendly which allows us to call #uniq on a
     # collection of them. See SelectManager#order for more details.
     class Ordering < Arel::Nodes::Unary
-      def hash
-        expr.hash
-      end
-      def ==(other)
-        other.is_a?(Arel::Nodes::Ordering) && self.expr == other.expr
-      end
       def eql?(other)
-        self == other
+        #Arel::Nodes::Ascending or Arel::Nodes::Desecnding
+        other.is_a?(Arel::Nodes::Ordering) &&
+        self.expr == other.expr
       end
+      alias :== :eql?
     end
-
   end
 end
