0fb4f2c0659059bbee4403d60c508f5f4937e0cf
jmozmoz <Joachim.Herb@gmx.de>
2014-08-27 23:19:21 +0200

    Add fix for issue #352 of sql server adapter and https://github.com/composite-primary-keys/composite_primary_keys/issues/224

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -298,7 +298,9 @@ module ActiveRecord
         def sql_for_insert(sql, pk, id_value, sequence_name, binds)
           sql =
             if pk
-              sql.insert(sql.index(/ (DEFAULT )?VALUES/), " OUTPUT inserted.#{pk}")
+              # support composite primary keys consisting of more than one column name
+              inserted_pks = [pk].flatten.map {|pk| "inserted.#{pk}"}
+              sql.insert(sql.index(/ (DEFAULT )?VALUES/), " OUTPUT #{inserted_pks.join(", ")}")
             else
               "#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident"
             end
