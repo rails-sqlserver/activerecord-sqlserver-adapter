b36440080b7d7ae2c66930d606a664b9c7e5a96c
Ken Collins <ken@metaskills.net>
2015-01-11 15:42:45 -0500

    Remove no-op quoting of datetime in cast object.

--- lib/active_record/connection_adapters/sqlserver/type/datetime.rb
+++ lib/active_record/connection_adapters/sqlserver/type/datetime.rb
@@ -6,13 +6,6 @@ module ActiveRecord
 
           include Castable
 
-          def type_cast_for_database(value)
-            value = super(value)
-            return unless value
-            return value unless value =~ ConnectionAdapters::Column::Format::ISO_DATETIME
-            "#{value.to_s(:db)}#{cast_usec_for_database(value)}"
-          end
-
 
           private
 
@@ -27,11 +20,6 @@ module ActiveRecord
             seconds = value.usec.to_f / 1_000_000.0
             ss_seconds = ((seconds * (1 / second_precision)).round / (1 / second_precision)).round(3)
             (ss_seconds * 1_000_000).to_i
-          end
-
-          def cast_usec_for_database(value)
-            usec = cast_usec(value)
-            '.' + (BigDecimal(usec.to_s) / 1_000_000).round(3).to_s.split('.').last
           end
 
           def second_precision
