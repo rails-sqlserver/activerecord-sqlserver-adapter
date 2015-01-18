54254f95c9dd4a335cdb0aa7292ce53dc4ae825d
Ken Collins <ken@metaskills.net>
2015-01-18 12:12:17 -0500

    Prepare whitespace for UUID.

--- test/cases/schema_dumper_test_sqlserver.rb
+++ test/cases/schema_dumper_test_sqlserver.rb
@@ -10,39 +10,39 @@ class SchemaDumperTestSQLServer < ActiveRecord::TestCase
   it 'sst_datatypes' do
     generate_schema_for_table 'sst_datatypes'
     # Exact Numerics
-    assert_line :bigint,        type: 'bigint',       limit: '8',           precision: nil,   scale: nil,  default: '42'
-    assert_line :int,           type: 'integer',      limit: '4',           precision: nil,   scale: nil,  default: '42'
-    assert_line :smallint,      type: 'integer',      limit: '2',           precision: nil,   scale: nil,  default: '42'
-    assert_line :tinyint,       type: 'integer',      limit: '1',           precision: nil,   scale: nil,  default: '42'
-    assert_line :bit,           type: 'boolean',      limit: nil,           precision: nil,   scale: nil,  default: 'true'
-    assert_line :decimal_9_2,   type: 'decimal',      limit: nil,           precision: '9',   scale: '2',  default: '12345.01'
-    assert_line :numeric_18_0,  type: 'decimal',      limit: nil,           precision: '18',  scale: '0',  default: '191.0'
-    assert_line :numeric_36_2,  type: 'decimal',      limit: nil,           precision: '36',  scale: '2',  default: '12345678901234567890.01'
-    assert_line :money,         type: 'money',        limit: nil,           precision: '19',  scale: '4',  default: '4.2'
-    assert_line :smallmoney,    type: 'smallmoney',   limit: nil,           precision: '10',  scale: '4',  default: '4.2'
+    assert_line :bigint,            type: 'bigint',       limit: '8',           precision: nil,   scale: nil,  default: '42'
+    assert_line :int,               type: 'integer',      limit: '4',           precision: nil,   scale: nil,  default: '42'
+    assert_line :smallint,          type: 'integer',      limit: '2',           precision: nil,   scale: nil,  default: '42'
+    assert_line :tinyint,           type: 'integer',      limit: '1',           precision: nil,   scale: nil,  default: '42'
+    assert_line :bit,               type: 'boolean',      limit: nil,           precision: nil,   scale: nil,  default: 'true'
+    assert_line :decimal_9_2,       type: 'decimal',      limit: nil,           precision: '9',   scale: '2',  default: '12345.01'
+    assert_line :numeric_18_0,      type: 'decimal',      limit: nil,           precision: '18',  scale: '0',  default: '191.0'
+    assert_line :numeric_36_2,      type: 'decimal',      limit: nil,           precision: '36',  scale: '2',  default: '12345678901234567890.01'
+    assert_line :money,             type: 'money',        limit: nil,           precision: '19',  scale: '4',  default: '4.2'
+    assert_line :smallmoney,        type: 'smallmoney',   limit: nil,           precision: '10',  scale: '4',  default: '4.2'
     # Approximate Numerics
-    assert_line :float,         type: 'float',        limit: nil,          precision: nil,   scale: nil,  default: '123.00000001'
-    assert_line :real,          type: 'real',         limit: nil,          precision: nil,   scale: nil,  default: %r{123.4[45]}
+    assert_line :float,             type: 'float',        limit: nil,          precision: nil,   scale: nil,  default: '123.00000001'
+    assert_line :real,              type: 'real',         limit: nil,          precision: nil,   scale: nil,  default: %r{123.4[45]}
     # Date and Time
-    assert_line :date,          type: 'date',         limit: nil,           precision: nil,   scale: nil,  default: "'0001-01-01'"
-    assert_line :datetime,      type: 'datetime',     limit: nil,           precision: nil,   scale: nil,  default: "'1753-01-01 00:00:00'"
-    assert_line :smalldatetime, type: 'datetime',     limit: nil,           precision: nil,   scale: nil,  default: "'1901-01-01 15:45:00'"
-    assert_line :time_2,        type: 'time',         limit: nil,           precision: '2',   scale: nil,  default: nil
-    assert_line :time_7,        type: 'time',         limit: nil,           precision: '7',   scale: nil,  default: nil
+    assert_line :date,              type: 'date',         limit: nil,           precision: nil,   scale: nil,  default: "'0001-01-01'"
+    assert_line :datetime,          type: 'datetime',     limit: nil,           precision: nil,   scale: nil,  default: "'1753-01-01 00:00:00'"
+    assert_line :smalldatetime,     type: 'datetime',     limit: nil,           precision: nil,   scale: nil,  default: "'1901-01-01 15:45:00'"
+    assert_line :time_2,            type: 'time',         limit: nil,           precision: '2',   scale: nil,  default: nil
+    assert_line :time_7,            type: 'time',         limit: nil,           precision: '7',   scale: nil,  default: nil
     # Character Strings
-    assert_line :char_10,       type: 'char',         limit: '10',          precision: nil,   scale: nil,  default: "\"1234567890\""
-    assert_line :varchar_50,    type: 'varchar',      limit: '50',          precision: nil,   scale: nil,  default: "\"test varchar_50\""
-    assert_line :varchar_max,   type: 'varchar_max',  limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test varchar_max\""
-    assert_line :text,          type: 'text_basic',   limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test text\""
+    assert_line :char_10,           type: 'char',         limit: '10',          precision: nil,   scale: nil,  default: "\"1234567890\""
+    assert_line :varchar_50,        type: 'varchar',      limit: '50',          precision: nil,   scale: nil,  default: "\"test varchar_50\""
+    assert_line :varchar_max,       type: 'varchar_max',  limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test varchar_max\""
+    assert_line :text,              type: 'text_basic',   limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test text\""
     # Unicode Character Strings
-    assert_line :nchar_10,      type: 'nchar',        limit: '10',          precision: nil,   scale: nil,  default: "\"12345678åå\""
-    assert_line :nvarchar_50,   type: 'string',       limit: '50',          precision: nil,   scale: nil,  default: "\"test nvarchar_50 åå\""
-    assert_line :nvarchar_max,  type: 'text',         limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test nvarchar_max åå\""
-    assert_line :ntext,         type: 'ntext',        limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test ntext åå\""
+    assert_line :nchar_10,          type: 'nchar',        limit: '10',          precision: nil,   scale: nil,  default: "\"12345678åå\""
+    assert_line :nvarchar_50,       type: 'string',       limit: '50',          precision: nil,   scale: nil,  default: "\"test nvarchar_50 åå\""
+    assert_line :nvarchar_max,      type: 'text',         limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test nvarchar_max åå\""
+    assert_line :ntext,             type: 'ntext',        limit: '2147483647',  precision: nil,   scale: nil,  default: "\"test ntext åå\""
     # Binary Strings
-    assert_line :binary_49,     type: 'binary_basic', limit: '49',          precision: nil,   scale: nil,  default: nil
-    assert_line :varbinary_49,  type: 'varbinary',    limit: '49',          precision: nil,   scale: nil,  default: nil
-    assert_line :varbinary_max, type: 'binary',       limit: '2147483647',  precision: nil,   scale: nil,  default: nil
+    assert_line :binary_49,         type: 'binary_basic', limit: '49',          precision: nil,   scale: nil,  default: nil
+    assert_line :varbinary_49,      type: 'varbinary',    limit: '49',          precision: nil,   scale: nil,  default: nil
+    assert_line :varbinary_max,     type: 'binary',       limit: '2147483647',  precision: nil,   scale: nil,  default: nil
   end
 
   it 'sst_datatypes_migration' do
