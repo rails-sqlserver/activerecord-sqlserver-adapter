ba63c413be2ab5b83a92df2bcc2eb29b20ca5274
Ken Collins <ken@metaskills.net>
2015-01-23 14:44:05 -0500

    Coerce tests.
    ActiveRecord::AdapterTest#test_update_prepared_statement
    BelongsToAssociationsTest#test_belongs_to_does_not_use_order_by
    BelongsToAssociationsTest#test_belongs_to_with_primary_key_joins_on_correct_column
    EagerAssociationTest#test_0013_including association based on sql condition and no database column
    EagerAssociationTest#test_count_with_include:
    AttributeMethodsTest#test_typecast_attribute_from_select_to_false
    AttributeMethodsTest#test_typecast_attribute_from_select_to_true
    BasicsTest#test_column_names_are_escaped:
    
    29 failures, 17 errors

--- test/cases/coerced_tests.rb
+++ test/cases/coerced_tests.rb
@@ -1,37 +1,70 @@
 require 'cases/helper_sqlserver'
 
 
-require 'models/post'
-class SanitizeTest < ActiveRecord::TestCase
+module ActiveRecord
+  class AdapterTest < ActiveRecord::TestCase
 
-  coerce_tests! :test_sanitize_sql_like_example_use_case
-  def test_sanitize_sql_like_example_use_case_coerced
-    searchable_post = Class.new(Post) do
-      def self.search(term)
-        where("title LIKE ?", sanitize_sql_like(term, '!'))
-      end
-    end
-    assert_sql(/\(title LIKE N''20!% !_reduction!_!!''\)/) do
-      searchable_post.search("20% _reduction_!").to_a
-    end
+    # As far as I can tell, SQL Server does not support null bytes in strings.
+    coerce_tests! :test_update_prepared_statement
+
+  end
+end
+
+
+
+
+require 'models/topic'
+class AttributeMethodsTest < ActiveRecord::TestCase
+
+  coerce_tests! :test_typecast_attribute_from_select_to_false
+  def test_typecast_attribute_from_select_to_false_coerced
+    Topic.create(:title => 'Budget')
+    topic = Topic.all.merge!(:select => "topics.*, IIF (1 = 2, 1, 0) as is_test").first
+    assert !topic.is_test?
+  end
+
+  coerce_tests! :test_typecast_attribute_from_select_to_true
+  def test_typecast_attribute_from_select_to_true_coerced
+    Topic.create(:title => 'Budget')
+    topic = Topic.all.merge!(:select => "topics.*, IIF (1 = 1, 1, 0) as is_test").first
+    assert topic.is_test?
   end
 
 end
 
 
 
-require 'models/author'
-class YamlSerializationTest < ActiveRecord::TestCase
 
-  coerce_tests! :test_types_of_virtual_columns_are_not_changed_on_round_trip
-  def test_types_of_virtual_columns_are_not_changed_on_round_trip_coerced
-    author = Author.select('authors.*, 5 as posts_count').first
-    dumped = YAML.load(YAML.dump(author))
-    assert_equal 5, author.posts_count
-    assert_equal 5, dumped.posts_count
+class BasicsTest < ActiveRecord::TestCase
+
+  coerce_tests! :test_column_names_are_escaped
+  def test_column_names_are_escaped_coerced
+    conn      = ActiveRecord::Base.connection
+    classname = conn.class.name[/[^:]*$/]
+    badchar   = "'"
+    quoted = conn.quote_column_name "foo#{badchar}bar"
+    assert_equal("#{badchar}foo#{badchar * 2}bar#{badchar}", quoted)
   end
 
 end
+
+
+
+
+class BelongsToAssociationsTest < ActiveRecord::TestCase
+
+  # Since @client.firm is a single first/top, and we use FETCH the order clause is used.
+  coerce_tests! :test_belongs_to_does_not_use_order_by
+
+  coerce_tests! :test_belongs_to_with_primary_key_joins_on_correct_column
+  def test_belongs_to_with_primary_key_joins_on_correct_column_coerced
+    sql = Client.joins(:firm_with_primary_key).to_sql
+    assert_no_match(/\[firm_with_primary_keys_companies\]\.\[id\]/, sql)
+    assert_match(/\[firm_with_primary_keys_companies\]\.\[name\]/, sql)
+  end
+
+end
+
 
 
 
@@ -49,64 +82,6 @@ module ActiveRecord
   end
 end
 
-
-
-require 'models/post'
-module ActiveRecord
-  class WhereChainTest < ActiveRecord::TestCase
-
-    coerce_tests! :test_not_eq_with_array_parameter
-    def test_not_eq_with_array_parameter_coerced
-      expected = Arel::Nodes::Not.new("title = N'hello'")
-      relation = Post.where.not(['title = ?', 'hello'])
-      assert_equal([expected], relation.where_values)
-    end
-
-  end
-end
-
-
-
-require 'models/company'
-class InheritanceTest < ActiveRecord::TestCase
-
-  coerce_tests! :test_a_bad_type_column
-  def test_a_bad_type_column_coerced
-    Company.connection.with_identity_insert_enabled('companies') do
-      Company.connection.insert "INSERT INTO companies (id, #{QUOTED_TYPE}, name) VALUES(100, 'bad_class!', 'Not happening')"
-    end
-    assert_raise(ActiveRecord::SubclassNotFound) { Company.find(100) }
-  end
-
-  coerce_tests! :test_eager_load_belongs_to_primary_key_quoting
-  def test_eager_load_belongs_to_primary_key_quoting_coerced
-    con = Account.connection
-    assert_sql(/\[companies\]\.\[id\] IN \(1\)/) do
-      Account.all.merge!(:includes => :firm).find(1)
-    end
-  end
-
-end
-
-
-
-require 'models/developer'
-require 'models/computer'
-class NestedRelationScopingTest < ActiveRecord::TestCase
-
-  coerce_tests! :test_merge_options
-  def test_merge_options_coerced
-    Developer.where('salary = 80000').scoping do
-      Developer.limit(10).scoping do
-        devs = Developer.all
-        sql = devs.to_sql
-        assert_match '(salary = 80000)', sql
-        assert_match 'FETCH NEXT 10 ROWS ONLY', sql
-      end
-    end
-  end
-
-end
 
 
 
@@ -129,33 +104,30 @@ end
 
 
 
-require 'models/topic'
-module ActiveRecord
-  class PredicateBuilderTest < ActiveRecord::TestCase
 
-    coerce_tests! :test_registering_new_handlers
-    def test_registering_new_handlers_coerced
-      PredicateBuilder.register_handler(Regexp, proc do |column, value|
-        Arel::Nodes::InfixOperation.new('~', column, Arel.sql(value.source))
-      end)
-      assert_match %r{\[topics\]\.\[title\] ~ rails}i, Topic.where(title: /rails/).to_sql
-    end
+require 'models/owner'
+class Owner < ActiveRecord::Base
+  scope :including_last_pet, -> {
+    select('owners.*, (select TOP (1) p.pet_id from pets p where p.owner_id = owners.owner_id order by p.name desc ) as last_pet_id').
+    includes(:last_pet)
+  }
+end
+class EagerAssociationTest < ActiveRecord::TestCase
 
+  # Use LEN() vs length() function.
+  coerce_tests! :test_count_with_include
+  def test_count_with_include_coerced
+    assert_equal 3, authors(:david).posts_with_comments.where("LEN(comments.body) > 15").references(:comments).count
   end
+
+  # Use TOP (1) in scope vs limit 1.
+  coerce_tests! %r{including association based on sql condition and no database column}
+  it "including association based on sql condition and no database column coerced" do
+    assert_equal pets(:parrot), Owner.including_last_pet.first.last_pet
+  end
+
 end
 
-
-
-module ActiveRecord
-  class Migration
-    class ChangeSchemaTest < ActiveRecord::TestCase
-
-      coerce_tests! :test_create_table_with_bigint,
-                    :test_create_table_with_defaults # We test these.
-
-    end
-  end
-end
 
 
 
@@ -189,9 +161,141 @@ end
 
 
 
+
+require 'models/company'
+class InheritanceTest < ActiveRecord::TestCase
+
+  coerce_tests! :test_a_bad_type_column
+  def test_a_bad_type_column_coerced
+    Company.connection.with_identity_insert_enabled('companies') do
+      Company.connection.insert "INSERT INTO companies (id, #{QUOTED_TYPE}, name) VALUES(100, 'bad_class!', 'Not happening')"
+    end
+    assert_raise(ActiveRecord::SubclassNotFound) { Company.find(100) }
+  end
+
+  coerce_tests! :test_eager_load_belongs_to_primary_key_quoting
+  def test_eager_load_belongs_to_primary_key_quoting_coerced
+    con = Account.connection
+    assert_sql(/\[companies\]\.\[id\] IN \(1\)/) do
+      Account.all.merge!(:includes => :firm).find(1)
+    end
+  end
+
+end
+
+
+
+
+module ActiveRecord
+  class Migration
+    class ChangeSchemaTest < ActiveRecord::TestCase
+
+       # We test these.
+      coerce_tests! :test_create_table_with_bigint,
+                    :test_create_table_with_defaults
+
+    end
+  end
+end
+
+
+
+
+require 'models/developer'
+require 'models/computer'
+class NestedRelationScopingTest < ActiveRecord::TestCase
+
+  coerce_tests! :test_merge_options
+  def test_merge_options_coerced
+    Developer.where('salary = 80000').scoping do
+      Developer.limit(10).scoping do
+        devs = Developer.all
+        sql = devs.to_sql
+        assert_match '(salary = 80000)', sql
+        assert_match 'FETCH NEXT 10 ROWS ONLY', sql
+      end
+    end
+  end
+
+end
+
+
+
+
+require 'models/topic'
+module ActiveRecord
+  class PredicateBuilderTest < ActiveRecord::TestCase
+
+    coerce_tests! :test_registering_new_handlers
+    def test_registering_new_handlers_coerced
+      PredicateBuilder.register_handler(Regexp, proc do |column, value|
+        Arel::Nodes::InfixOperation.new('~', column, Arel.sql(value.source))
+      end)
+      assert_match %r{\[topics\]\.\[title\] ~ rails}i, Topic.where(title: /rails/).to_sql
+    end
+
+  end
+end
+
+
+
+
 class RelationTest < ActiveRecord::TestCase
 
   coerce_tests! %r{doesn't have implicit ordering} # We have implicit ordering, via FETCH.
+
+end
+
+
+
+
+require 'models/post'
+class SanitizeTest < ActiveRecord::TestCase
+
+  coerce_tests! :test_sanitize_sql_like_example_use_case
+  def test_sanitize_sql_like_example_use_case_coerced
+    searchable_post = Class.new(Post) do
+      def self.search(term)
+        where("title LIKE ?", sanitize_sql_like(term, '!'))
+      end
+    end
+    assert_sql(/\(title LIKE N''20!% !_reduction!_!!''\)/) do
+      searchable_post.search("20% _reduction_!").to_a
+    end
+  end
+
+end
+
+
+
+
+require 'models/post'
+module ActiveRecord
+  class WhereChainTest < ActiveRecord::TestCase
+
+    coerce_tests! :test_not_eq_with_array_parameter
+    def test_not_eq_with_array_parameter_coerced
+      expected = Arel::Nodes::Not.new("title = N'hello'")
+      relation = Post.where.not(['title = ?', 'hello'])
+      assert_equal([expected], relation.where_values)
+    end
+
+  end
+end
+
+
+
+
+require 'models/author'
+class YamlSerializationTest < ActiveRecord::TestCase
+
+  coerce_tests! :test_types_of_virtual_columns_are_not_changed_on_round_trip
+  def test_types_of_virtual_columns_are_not_changed_on_round_trip_coerced
+    author = Author.select('authors.*, 5 as posts_count').first
+    dumped = YAML.load(YAML.dump(author))
+    assert_equal 5, author.posts_count
+    assert_equal 5, dumped.posts_count
+  end
 
 end
 
