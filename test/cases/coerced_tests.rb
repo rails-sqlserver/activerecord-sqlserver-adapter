4b0de7486d23b32daab862ce36b6206ea51fa1b2
Ken Collins <ken@metaskills.net>
2015-01-22 23:35:00 -0500

    Update coerced_tests format a bit.

--- test/cases/coerced_tests.rb
+++ test/cases/coerced_tests.rb
@@ -5,7 +5,6 @@ require 'models/post'
 class SanitizeTest < ActiveRecord::TestCase
 
   coerce_tests! :test_sanitize_sql_like_example_use_case
-
   def test_sanitize_sql_like_example_use_case_coerced
     searchable_post = Class.new(Post) do
       def self.search(term)
@@ -27,7 +26,6 @@ class YamlSerializationTest < ActiveRecord::TestCase
   fixtures :authors
 
   coerce_tests! :test_types_of_virtual_columns_are_not_changed_on_round_trip
-
   def test_types_of_virtual_columns_are_not_changed_on_round_trip_coerced
     author = Author.select('authors.*, 5 as posts_count').first
     dumped = YAML.load(YAML.dump(author))
@@ -41,27 +39,15 @@ end
 
 module ActiveRecord
   module ConnectionAdapters
-    class TypeLookupTest < ActiveRecord::TestCase
 
-      coerce_all_tests! # Just like PostgreSQLAdapter does.
+    # Just like PostgreSQLAdapter does.
+    TypeLookupTest.coerce_all_tests! if defined?(TypeLookupTest)
 
-    end
-  end
-end
+    # All sorts of errors due to how we test. Even setting ENV['RAILS_ENV'] to
+    # a value of 'default_env' will still show tests failing. Just ignoring all
+    # of them since we have no monkey in this circus.
+    MergeAndResolveDefaultUrlConfigTest.coerce_all_tests! if defined?(MergeAndResolveDefaultUrlConfigTest)
 
-
-
-module ActiveRecord
-  module ConnectionAdapters
-    class MergeAndResolveDefaultUrlConfigTest < ActiveRecord::TestCase
-
-      # All sorts of errors due to how we test. Even setting ENV['RAILS_ENV'] to
-      # a value of 'default_env' will still show tests failing. Just ignoring all
-      # of them since we have no monkey in this circus.
-      #
-      coerce_all_tests!
-
-    end
   end
 end
 
@@ -208,5 +194,4 @@ class RelationTest < ActiveRecord::TestCase
   coerce_tests! %r{doesn't have implicit ordering} # We have implicit ordering, via FETCH.
 
 end
-
 
