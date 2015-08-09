095abf79fb42e168cc0d6cddaa3e6c20496df125
Ken Collins <ken@metaskills.net>
2015-08-09 15:45:59 -0400

    [Appveyor] Create DB user before giving roles.

--- test/appveyor/dbsetup.sql
+++ test/appveyor/dbsetup.sql
@@ -2,6 +2,7 @@ CREATE DATABASE [activerecord_unittest];
 CREATE DATABASE [activerecord_unittest2];
 GO
 CREATE LOGIN [rails] WITH PASSWORD = '', CHECK_POLICY = OFF, DEFAULT_DATABASE = [activerecord_unittest];
+GO
 EXEC sp_addrolemember N'sysadmin', N'rails';
 GO
 USE [activerecord_unittest];
