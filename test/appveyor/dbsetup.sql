77712a7cb9fe7829086aed4a6dab6c47d43716f7
Ken Collins <ken@metaskills.net>
2015-08-09 15:51:25 -0400

    [Appveyor] Assign all DB roles at one time.

--- test/appveyor/dbsetup.sql
+++ test/appveyor/dbsetup.sql
@@ -3,10 +3,9 @@ CREATE DATABASE [activerecord_unittest2];
 GO
 CREATE LOGIN [rails] WITH PASSWORD = '', CHECK_POLICY = OFF, DEFAULT_DATABASE = [activerecord_unittest];
 GO
-EXEC sp_addrolemember N'sysadmin', N'rails';
-GO
 USE [activerecord_unittest];
 CREATE USER [rails] FOR LOGIN [rails];
 GO
 EXEC sp_addrolemember N'db_owner', N'rails';
+EXEC sp_addrolemember N'sysadmin', N'rails';
 GO
