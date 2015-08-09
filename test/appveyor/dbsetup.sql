d78de3fa0dfb20e9eea6514bab7d2dcdc5fba10f
Ken Collins <ken@metaskills.net>
2015-08-09 15:57:35 -0400

    [Appveyor] Better SP to add sysadmin role.

--- test/appveyor/dbsetup.sql
+++ test/appveyor/dbsetup.sql
@@ -7,5 +7,5 @@ USE [activerecord_unittest];
 CREATE USER [rails] FOR LOGIN [rails];
 GO
 EXEC sp_addrolemember N'db_owner', N'rails';
-EXEC sp_addrolemember N'sysadmin', N'rails';
+EXEC master..sp_addsrvrolemember @loginame = N'rails', @rolename = N'sysadmin'
 GO
