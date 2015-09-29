a44f7a763a36cdb1e967883cfe84698a99771aef
Faisal Mansoor <fmansoor@practicefusion.com>
2015-09-28 18:00:36 -0700

    allow running sql batch with a given isolation level

--- lib/active_record/sqlserver_base.rb
+++ lib/active_record/sqlserver_base.rb
@@ -18,5 +18,24 @@ module ActiveRecord
       ConnectionAdapters::SQLServerAdapter.new(nil, logger, nil, config.merge(mode: mode))
     end
 
+    # Runs a batch of sql statements with an isolation_level
+    #
+    # For example:
+    #
+    #   ActiveRecord::Base.with_isolation_level(:read_uncommitted) do
+    #     Posts.where(id: 10).first
+    #   end
+    #
+    # This example will change the select query with :read_uncommitted isolation level
+    #
+    def self.with_isolation_level(isolation_level)
+      old_isolation_level_str = connection.user_options_isolation_level.to_s
+      isolation_level_str = connection.transaction_isolation_levels.fetch(isolation_level)
+      connection.execute "SET TRANSACTION ISOLATION LEVEL #{isolation_level_str}"
+      yield
+    ensure
+      connection.execute "SET TRANSACTION ISOLATION LEVEL #{old_isolation_level_str}"
+    end
+
   end
 end
