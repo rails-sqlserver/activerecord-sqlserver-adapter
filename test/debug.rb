b85b36fac9a686a52bc4d971635128ff0b891d15
Ken Collins <ken@metaskills.net>
2016-02-07 17:56:30 -0500

    Debug

--- test/debug.rb
+++ test/debug.rb
@@ -1,5 +1,7 @@
-# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-# require 'bundler/setup' ; Bundler.require :default, :development
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require 'bundler/setup' ; Bundler.require :default, :development
+
+sleep 5
 require 'tiny_tds'
 
 c = TinyTds::Client.new(
