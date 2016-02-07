aa0f197316a3837bbde9d2ecd195652be969eacd
Ken Collins <ken@metaskills.net>
2016-02-07 11:07:43 -0500

    Debug

---
+++ test/debug.rb
@@ -0,0 +1,12 @@
+require 'tiny_tds'
+
+c = TinyTds::Client.new(
+  host: ENV['CI_AZURE_HOST'],
+  username: 'rails',
+  password: ENV['CI_AZURE_PASS'],
+  database: 'activerecord_unittest',
+  azure: true
+)
+
+puts c.execute("SELECT 1 AS [one]").each
+c.close
