fba89d4a323495936b49249631641c1e468af6dd
Uday Kadaboina <uday88k@gmail.com>
2016-10-04 20:19:18 -0400

    added support for MSSQL2016

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -181,7 +181,7 @@ module ActiveRecord
       VERSION                     = File.read(File.expand_path("../../../../VERSION",__FILE__)).strip
       ADAPTER_NAME                = 'SQLServer'.freeze
       DATABASE_VERSION_REGEXP     = /Microsoft SQL Server\s+"?(\d{4}|\w+)"?/
-      SUPPORTED_VERSIONS          = [2005,2008,2010,2011,2012,2014]
+      SUPPORTED_VERSIONS          = [2005,2008,2010,2011,2012,2014,2016]
 
       attr_reader :database_version, :database_year, :spid, :product_level, :product_version, :edition
 
@@ -334,6 +334,10 @@ module ActiveRecord
 
       def sqlserver_2012?
         @database_year == 2012
+      end
+
+      def sqlserver_2016?
+        @database_year == 2016
       end
 
       def sqlserver_azure?
