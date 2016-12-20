cac85f530c2aae899f74849429b2bda910ee2d7f
Ken Collins <ken@metaskills.net>
2016-12-19 21:57:03 -0500

    Reduce Appveyor. Add CircleCI & TravisCI.
    Removed `-x64` appveyor build versions till TinyTDS v1.1 is out.

---
+++ test/bin/install-freetds.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+FREETDS_VERSION=1.00.21
+
+wget ftp://ftp.freetds.org/pub/freetds/stable/freetds-$FREETDS_VERSION.tar.gz
+tar -xzf freetds-$FREETDS_VERSION.tar.gz
+cd freetds-$FREETDS_VERSION
+./configure --prefix=/opt/local \
+            --with-openssl=/opt/local \
+            --with-tdsver=7.3
+make
+make install
+cd ..
+rm -rf freetds-$FREETDS_VERSION
+rm freetds-$FREETDS_VERSION.tar.gz
---
+++ test/bin/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+docker pull metaskills/mssql-server-linux-rails
+
+container=$(docker ps -a -q --filter ancestor=metaskills/mssql-server-linux-rails)
+if [[ -z $container ]]; then
+  docker run -p 1433:1433 -d metaskills/mssql-server-linux-rails && sleep 10
+  exit
+fi
+
+container=$(docker ps -q --filter ancestor=metaskills/mssql-server-linux-rails)
+if [[ -z $container ]]; then
+  docker start $container && sleep 10
+fi
