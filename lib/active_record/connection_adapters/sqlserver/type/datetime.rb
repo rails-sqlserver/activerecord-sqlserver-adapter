dc25b61295f3e6ffb516a02f75796c76a097e726
Ken Collins <ken@metaskills.net>
2017-01-07 17:34:38 -0500

    [Rails5] Pass more attribute tests for date/time/zones.

--- lib/active_record/connection_adapters/sqlserver/type/datetime.rb
+++ lib/active_record/connection_adapters/sqlserver/type/datetime.rb
@@ -11,8 +11,9 @@ module ActiveRecord
           end
 
           def serialize(value)
-            return super unless value.acts_like?(:time)
-            datetime = super.to_s(:_sqlserver_datetime).tap do |v|
+            value = super
+            return value unless value.acts_like?(:time)
+            datetime = value.to_s(:_sqlserver_datetime).tap do |v|
               fraction = quote_fractional(value)
               v << ".#{fraction}"
             end
@@ -34,9 +35,7 @@ module ActiveRecord
           private
 
           def cast_value(value)
-            value = value.acts_like?(:time) ? value : super
-            return unless value
-            apply_seconds_precision(value)
+            super.try(:in_time_zone)
           end
 
           def fast_string_to_time(string)
@@ -51,6 +50,20 @@ module ActiveRecord
 
           def fast_string_to_time_zone
             ::Time.zone || ActiveSupport::TimeZone.new('UTC')
+          end
+
+          # Copy of module ActiveModel::Type::Helpers::TimeValue with
+          # else condition using a zone for local parsing.
+          def new_time(year, mon, mday, hour, min, sec, microsec, offset = nil)
+            return if year.nil? || (year == 0 && mon == 0 && mday == 0)
+            if offset
+              time = ::Time.utc(year, mon, mday, hour, min, sec, microsec) rescue nil
+              return unless time
+              time -= offset
+              is_utc? ? time : time.getlocal
+            else
+              fast_string_to_time_zone.local(year, mon, mday, hour, min, sec, microsec) rescue nil
+            end
           end
 
         end
