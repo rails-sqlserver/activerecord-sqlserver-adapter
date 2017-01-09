0c403d6fb05f7e3ebb56cff9604316b6334eeefe
Ken Collins <ken@metaskills.net>
2017-01-08 19:09:39 -0500

    [Rails5] Pass MigrationTest#test_filtering_migrations.

--- lib/arel/visitors/sqlserver.rb
+++ lib/arel/visitors/sqlserver.rb
@@ -190,7 +190,7 @@ module Arel
 
       def primary_Key_From_Table t
         return unless t
-        column_name = schema_cache.primary_keys(t.name) || column_cache(t.name).first.second.try(:name)
+        column_name = schema_cache.primary_keys(t.name) || column_cache(t.name).first.try(:second).try(:name)
         column_name ? t[column_name] : nil
       end
 
