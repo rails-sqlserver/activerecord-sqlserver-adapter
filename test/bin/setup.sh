bfe3e14b683899a2c90b013c9e8fcbee61ef98f0
Ken Collins <ken@metaskills.net>
2017-01-18 19:34:08 -0500

    Use "Microsoft SQL Server vNext (CTP1.1) - 14.0.100.187 (X64)""

--- test/bin/setup.sh
+++ test/bin/setup.sh
@@ -3,15 +3,17 @@
 set -x
 set -e
 
-docker pull metaskills/mssql-server-linux-rails
+tag=1.1
 
-container=$(docker ps -a -q --filter ancestor=metaskills/mssql-server-linux-rails)
+docker pull metaskills/mssql-server-linux-rails:$tag
+
+container=$(docker ps -a -q --filter ancestor=metaskills/mssql-server-linux-rails:$tag)
 if [[ -z $container ]]; then
-  docker run -p 1433:1433 -d metaskills/mssql-server-linux-rails && sleep 10
+  docker run -p 1433:1433 -d metaskills/mssql-server-linux-rails:$tag && sleep 10
   exit
 fi
 
-container=$(docker ps -q --filter ancestor=metaskills/mssql-server-linux-rails)
+container=$(docker ps -q --filter ancestor=metaskills/mssql-server-linux-rails:$tag)
 if [[ -z $container ]]; then
   docker start $container && sleep 10
 fi
