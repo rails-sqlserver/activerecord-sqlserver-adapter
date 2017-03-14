db038d33cdbd1dac5a8482edc604b50ac967dbaf
Ken Collins <ken@metaskills.net>
2017-03-13 23:18:49 -0400

    Fix up scratch file [ci skip]

--- test/cases/scratchpad_test_sqlserver.rb
+++ test/cases/scratchpad_test_sqlserver.rb
@@ -1,11 +1,8 @@
 require 'cases/helper_sqlserver'
-require 'models/book'
 
 class ScratchpadTestSQLServer < ActiveRecord::TestCase
 
   it 'helps debug things' do
-    $FOO = true
-    1000.times { Book.create! name: 'test' }
   end
 
 end
