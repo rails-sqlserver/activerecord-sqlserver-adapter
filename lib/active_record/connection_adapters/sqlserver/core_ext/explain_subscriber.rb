5515114ea563b3242ec5310a3b52998ecb1168a6
Michaël Rigart <michael@netronix.be>
2017-05-31 08:02:41 +0200

    override constant value after AR is loaded

--- lib/active_record/connection_adapters/sqlserver/core_ext/explain_subscriber.rb
+++ lib/active_record/connection_adapters/sqlserver/core_ext/explain_subscriber.rb
@@ -1,4 +1,6 @@
-silence_warnings do
-  # Already defined in Rails
-  ActiveRecord::ExplainSubscriber::EXPLAINED_SQLS = /(select|update|delete|insert)\b/i
+ActiveSupport.on_load(:active_record) do
+  silence_warnings do
+    # Already defined in Rails
+    ActiveRecord::ExplainSubscriber::EXPLAINED_SQLS = /(select|update|delete|insert)\b/i
+  end
 end
