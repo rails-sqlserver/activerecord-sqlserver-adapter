9a6f5b9bbf3020e761b837e7fa6ff0494b502d1c
Matthew Dunbar <matthew.dunbar@lifeway.com>
2018-06-27 15:44:20 -0400

    Update JSON base class

--- lib/active_record/connection_adapters/sqlserver/type/json.rb
+++ lib/active_record/connection_adapters/sqlserver/type/json.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module ConnectionAdapters
     module SQLServer
       module Type
-        class Json < ActiveRecord::Type::Internal::AbstractJson
+        class Json < ActiveRecord::Type::Json
 
         end
       end
