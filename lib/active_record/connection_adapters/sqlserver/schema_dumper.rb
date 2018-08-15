86cec3f7fef474680e1326baac141a2af7e63ff6
Alex Gordon <agordon@sessionm.com>
2018-08-15 11:10:17 -0400

    this is what I got; potential fix for #317

--- lib/active_record/connection_adapters/sqlserver/schema_dumper.rb
+++ lib/active_record/connection_adapters/sqlserver/schema_dumper.rb
@@ -30,8 +30,21 @@ module ActiveRecord
         def default_primary_key?(column)
           super && column.is_primary? && column.is_identity?
         end
-
       end
+    end
+  end
+
+  class SchemaDumper
+    private
+
+    def remove_prefix_and_suffix(table)
+      table = remove_schema(table)
+      table.gsub(/^(#{@options[:table_name_prefix]})(.+)(#{@options[:table_name_suffix]})$/,  "\\2")
+    end
+
+    def remove_schema(table_with_schema)
+      identifier = ActiveRecord::ConnectionAdapters::SQLServer::Utils.extract_identifiers(table_with_schema)
+      identifier.object
     end
   end
 end
