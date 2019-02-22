dc4ca14e3d25d6f05b7eb32595c31fe05cd6776c
Lairton Lélis <lairton.lelis@codeminer42.com>
2019-02-22 19:54:49 -0300

    Add test cases for change_column_null bug

---
+++ test/cases/change_column_null_test_sqlserver.rb
@@ -0,0 +1,42 @@
+require 'cases/helper_sqlserver'
+require 'migrations/create_clients_and_change_column_null'
+
+class ChangeColumnNullTestSqlServer < ActiveRecord::TestCase
+  before do
+    @old_verbose = ActiveRecord::Migration.verbose
+    ActiveRecord::Migration.verbose = false
+    CreateClientsAndChangeColumnNull.new.up
+  end
+
+  after do
+    CreateClientsAndChangeColumnNull.new.down
+    ActiveRecord::Migration.verbose = @old_verbose
+  end
+
+  def find_column(table, name)
+    table.find { |column| column.name == name }
+  end
+
+  let(:clients_table) { connection.columns('clients') }
+  let(:name_column) { find_column(clients_table, 'name') }
+  let(:code_column) { find_column(clients_table, 'code') }
+  let(:value_column) { find_column(clients_table, 'value') }
+
+  describe '#change_column_null' do
+    it 'does not change the column limit' do
+      name_column.limit.must_equal 15
+    end
+
+    it 'does not change the column default' do
+      code_column.default.must_equal 'n/a'
+    end
+
+    it 'does not change the column precision' do
+      value_column.precision.must_equal 32
+    end
+
+    it 'does not change the column scale' do
+      value_column.scale.must_equal 8
+    end
+  end
+end
---
+++ test/migrations/create_clients_and_change_column_null.rb
@@ -0,0 +1,23 @@
+class CreateClientsAndChangeColumnNull < ActiveRecord::Migration[5.2]
+  def up
+    create_table :clients do |t|
+      t.string :name
+      t.string :code
+      t.decimal :value
+
+      t.timestamps
+    end
+
+    change_column :clients, :name, :string, limit: 15
+    change_column :clients, :code, :string, default: 'n/a'
+    change_column :clients, :value, :decimal, precision: 32, scale: 8
+
+    change_column_null :clients, :name, false
+    change_column_null :clients, :code, false
+    change_column_null :clients, :value, false
+  end
+
+  def down
+    drop_table :clients
+  end
+end
