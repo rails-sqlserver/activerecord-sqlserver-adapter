e929378d2f123e2993829c1a10c87929e2ebc83f
Aidan Haran <aidanharan@yahoo.com>
2020-02-27 11:56:49 +0000

    Added prepared statement caching

--- test/cases/coerced_tests.rb
+++ test/cases/coerced_tests.rb
@@ -145,8 +145,20 @@ end
 
 module ActiveRecord
   class BindParameterTest < ActiveRecord::TestCase
-    # Never finds `sql` since we use `EXEC sp_executesql` wrappers.
+    # Same as original coerced test except log is found using `EXEC sp_executesql` wrapper.
     coerce_tests! :test_binds_are_logged
+    def test_binds_are_logged_coerced
+      sub   = Arel::Nodes::BindParam.new(1)
+      binds = [Relation::QueryAttribute.new("id", 1, Type::Value.new)]
+      sql   = "select * from topics where id = #{sub.to_sql}"
+
+      @connection.exec_query(sql, "SQL", binds)
+
+      logged_sql = "EXEC sp_executesql N'#{sql}', N'#{sub.to_sql} int', #{sub.to_sql} = 1"
+      message = @subscriber.calls.find { |args| args[4][:sql] == logged_sql }
+
+      assert_equal binds, message[4][:binds]
+    end
   end
 end
 
