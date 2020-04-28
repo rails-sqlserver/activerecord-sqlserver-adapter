9afa4b303a6d4c01b067aedcd15f84c2bda23cb5
Yehuda Goldberg <hvusvd@gmail.com>
2020-04-28 10:29:45 +0300

    Coerce test an empty transaction does not raise if preventing writes. The transaction in the test become SavepointTransaction  because tests automatically wrapped in transaction, and SQL Server does not have query for release_savepoint

--- test/cases/coerced_tests.rb
+++ test/cases/coerced_tests.rb
@@ -177,6 +177,18 @@ class BasicsTest < ActiveRecord::TestCase
       end
     end
   end
+
+  # SQL Server does not have query for release_savepoint
+  coerce_tests! %r{an empty transaction does not raise if preventing writes}
+  test "an empty transaction does not raise if preventing writes coerced" do
+    ActiveRecord::Base.connection_handler.while_preventing_writes do
+      assert_queries(1, ignore_none: true) do
+        Bird.transaction do
+          ActiveRecord::Base.connection.materialize_transactions
+        end
+      end
+    end
+  end
 end
 
 
