b1e02f977e9873eacc755bd74a6d69ec2429ee86
Aidan Haran <aidanharan@yahoo.com>
2020-05-06 17:59:38 +0100

    Rails 6: Log subscriber workaround (#799)
    * Coerce and call original
    
    * Added comment

--- test/cases/coerced_tests.rb
+++ test/cases/coerced_tests.rb
@@ -1463,3 +1463,14 @@ class EagerLoadingTooManyIdsTest < ActiveRecord::TestCase
     end
   end
 end
+
+
+
+
+class LogSubscriberTest < ActiveRecord::TestCase
+  # Call original test from coerced test. Fixes issue on CI with Rails installed as a gem.
+  coerce_tests! :test_vebose_query_logs
+  def test_vebose_query_logs_coerced
+    original_test_vebose_query_logs
+  end
+end
