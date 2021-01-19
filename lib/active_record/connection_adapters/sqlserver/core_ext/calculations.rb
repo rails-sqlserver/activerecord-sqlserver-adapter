3399f41829d1e205b23e296acae457ab2332e2b3
Łukasz Kornek <lk0001@wp.pl>
2021-01-19 18:04:29 +0100

    fix for type_cast_calculated_value has already been applied to rails 5.2, so we don't need to it anymore

--- lib/active_record/connection_adapters/sqlserver/core_ext/calculations.rb
+++ lib/active_record/connection_adapters/sqlserver/core_ext/calculations.rb
@@ -31,15 +31,6 @@ module ActiveRecord
           def build_count_subquery(relation, column_name, distinct)
             super(relation.unscope(:order), column_name, distinct)
           end
-
-          def type_cast_calculated_value(value, type, operation = nil)
-            case operation
-            when "count"   then value.to_i
-            when "sum"     then type.deserialize(value || 0)
-            when "average" then value&.respond_to?(:to_d) ? value.to_d : value
-            else type.deserialize(value)
-            end
-          end
         end
       end
     end
