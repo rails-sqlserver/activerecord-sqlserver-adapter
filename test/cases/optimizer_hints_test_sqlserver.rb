54dd652f715aef36a762c9d93dced87b91e7a82d
Matias Grunberg <matias@yellowspot.dev>
2021-03-30 20:31:19 -0300

    add more specs

--- test/cases/optimizer_hints_test_sqlserver.rb
+++ test/cases/optimizer_hints_test_sqlserver.rb
@@ -20,6 +20,22 @@ class OptimizerHitsTestSQLServer < ActiveRecord::TestCase
     end
   end
 
+  it "apply multiple optimizations" do
+    assert_sql(%r{\ASELECT .+ FROM .+ OPTION \(HASH GROUP, FAST 1\)\z}) do
+      companies = Company.optimizer_hints("HASH GROUP", "FAST 1")
+      companies = companies.distinct.select("firm_id")
+      assert_includes companies.explain, "| Hash Match | Flow Distinct |"
+    end
+  end
+
+  it "support subqueries" do
+    assert_sql(%r{.*'SELECT COUNT\(count_column\) FROM \(SELECT .*\) subquery_for_count OPTION \(MAXDOP 2\)'.*}) do
+      companies = Company.optimizer_hints("MAXDOP 2")
+      companies = companies.select(:id).where(firm_id: [0, 1]).limit(3)
+      assert_equal 3, companies.count
+    end
+  end
+
   it "sanitize values" do
     assert_sql(%r{\ASELECT .+ FROM .+ OPTION \(HASH GROUP\)\z}) do
       companies = Company.optimizer_hints("OPTION (HASH GROUP)")
