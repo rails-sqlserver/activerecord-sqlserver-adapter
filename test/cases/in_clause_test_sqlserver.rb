059d244f3dbe988e850f71e87698d319ea0925a6
Aidan Haran <aidanharan@yahoo.com>
2021-04-19 21:09:24 +0100

    Fix removal of invalid ordering from select statements (#890)
    Co-authored-by: Aidan Haran <aharan@fusioneer.com>
    Co-authored-by: Wanderson Policarpo <wpolicarpo@gmail.com>

--- test/cases/in_clause_test_sqlserver.rb
+++ test/cases/in_clause_test_sqlserver.rb
@@ -33,4 +33,31 @@ class InClauseTestSQLServer < ActiveRecord::TestCase
     assert_includes posts.to_sql, "ORDER BY [authors].[name]"
     assert_equal 8, posts.length
   end
+
+  it "removes ordering from 'not' subqueries" do
+    authors_subquery = Author.where.not(name: ["Mary", "Bob"]).order(:name)
+    posts = Post.where(author: authors_subquery)
+
+    assert_includes authors_subquery.to_sql, "ORDER BY [authors].[name]"
+    assert_not_includes posts.to_sql, "ORDER BY [authors].[name]"
+    assert_equal 5, posts.length
+  end
+
+  it "does not remove ordering from 'not' subquery that includes a limit" do
+    authors_subquery = Author.where.not(name: ["Ronan", "Mary", "Bob"]).order(:name).limit(2)
+    posts = Post.where(author: authors_subquery)
+
+    assert_includes authors_subquery.to_sql, "ORDER BY [authors].[name]"
+    assert_includes posts.to_sql, "ORDER BY [authors].[name]"
+    assert_equal 5, posts.length
+  end
+
+  it "does not remove ordering from 'not' subquery that includes an offset" do
+    authors_subquery = Author.where.not(name: ["David", "Ronan", "Cian"]).order(:name).offset(1)
+    posts = Post.where(author: authors_subquery)
+
+    assert_includes authors_subquery.to_sql, "ORDER BY [authors].[name]"
+    assert_includes posts.to_sql, "ORDER BY [authors].[name]"
+    assert_equal 3, posts.length
+  end
 end
