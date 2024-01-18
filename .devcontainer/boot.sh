c4568b2d601482e1dfd8cd7e68bb755721373dd9
Aidan Haran <aidanharan@yahoo.com>
2024-01-18 15:24:44 +0000

    Cleanup

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,17 +1,9 @@
-#bundle install
-#yarn install
-
 sudo chown -R vscode:vscode /usr/local/bundle
 
-#cd activerecord
-#
-## Create PostgreSQL databases
-#bundle exec rake db:postgresql:rebuild
-#
-## Create MySQL databases
-#MYSQL_CODESPACES=1 bundle exec rake db:mysql:rebuild
+# Wait for 5 seconds to make sure SQL Server came up.
+sleep 5
 
-
+# Setup test databases and users.
 /opt/mssql-tools18/bin/sqlcmd -C -S sqlserver -U sa -P "MSSQLadmin!" <<SQL
 CREATE DATABASE [activerecord_unittest];
 CREATE DATABASE [activerecord_unittest2];
@@ -25,3 +17,6 @@ EXEC sp_addrolemember N'db_owner', N'rails';
 EXEC master..sp_addsrvrolemember @loginame = N'rails', @rolename = N'sysadmin';
 GO
 SQL
+
+# Mark directory as safe in Git so that commands run without warnings.
+git config --global --add safe.directory /workspaces/activerecord-sqlserver-adapter
