f34bfc0506fca13cba3ad5d27afe6f1cb81fc8a7
Aidan Haran <aidanharan@yahoo.com>
2024-01-23 11:59:17 +0000

    Wait longer to make sure database is up

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,7 +1,7 @@
 sudo chown -R vscode:vscode /usr/local/bundle
 
-# Wait for 5 seconds to make sure SQL Server came up.
-sleep 5
+# Wait for 10 seconds to make sure SQL Server came up.
+sleep 10
 
 # Setup test databases and users.
 /opt/mssql-tools18/bin/sqlcmd -C -S sqlserver -U sa -P "MSSQLadmin!" <<SQL
