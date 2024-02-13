c0a96c39e63a30b6e6cc4188b0714ed52f088877
Jon Hinson <hinson.jonathan@gmail.com>
2024-02-13 09:49:43 -0600

    Composite Key Inserts with Triggers
    This updates the `OUTPUT INSERTED` statement to handle the case of
    composite primary keys being properly returned when the table also has
    a trigger.

--- test/models/sqlserver/trigger.rb
+++ test/models/sqlserver/trigger.rb
@@ -7,3 +7,7 @@ end
 class SSTestTriggerUuid < ActiveRecord::Base
   self.table_name = "sst_table_with_uuid_trigger"
 end
+
+class SSTestTriggerCompositePk < ActiveRecord::Base
+  self.table_name = "sst_table_with_composite_pk_trigger"
+end
