0156a95c4f0f9f07195f44d6a2942e84ad9de268
vividmuimui <1803598+vividmuimui@users.noreply.github.com>
2024-04-23 23:02:24 +0900

    Fix composite pk with defferent data type with triggers

--- test/models/sqlserver/trigger.rb
+++ test/models/sqlserver/trigger.rb
@@ -11,3 +11,7 @@ end
 class SSTestTriggerCompositePk < ActiveRecord::Base
   self.table_name = "sst_table_with_composite_pk_trigger"
 end
+
+class SSTestTriggerCompositePkWithDefferentDataType < ActiveRecord::Base
+  self.table_name = "sst_table_with_composite_pk_trigger_with_different_data_type"
+end
