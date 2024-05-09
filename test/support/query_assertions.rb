f1c61f7a27fad6d946034af5acbea4a26eeb02b2
Aidan Haran <aidanharan@yahoo.com>
2024-05-09 20:51:09 +0100

    Update query_assertions.rb

--- test/support/query_assertions.rb
+++ test/support/query_assertions.rb
@@ -13,9 +13,7 @@ module ARTest
           # Rails tests expect a save-point to be released at the end of the test. SQL Server does not release
           # save-points and so the number of queries will be off by one. This monkey patch adds a placeholder query
           # to the end of the queries array to account for the missing save-point release.
-          if queries.any? { |query| query =~ /SAVE TRANSACTION \S+/ }
-            queries.append "/* release savepoint placeholder for testing */"
-          end
+          queries.append "/* release savepoint placeholder for testing */" if queries.first =~ /SAVE TRANSACTION \S+/
           # End of monkey-patch
 
           if count
