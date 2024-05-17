6139c0faaf2550c05e03bb72b58cb806ec356994
Aidan Haran <aidanharan@yahoo.com>
2024-05-17 19:09:16 +0100

    Cleanup

--- test/support/query_assertions.rb
+++ test/support/query_assertions.rb
@@ -10,24 +10,7 @@ module ARTest
           queries = include_schema ? counter.log_all : counter.log
 
           # Start of monkey-patch
-          # Rails tests expect a save-point to be created and released. SQL Server does not release
-          # save-points and so the number of queries will be off. This monkey patch adds a placeholder queries
-          # to replace the missing save-point releases.
-          grouped_queries = [[]]
-
-          queries.each do |query|
-            if query =~ /SAVE TRANSACTION \S+/
-              grouped_queries << [query]
-            else
-              grouped_queries.last << query
-            end
-          end
-
-          grouped_queries.each do |group|
-            group.append "/* release savepoint placeholder for testing */" if group.first =~ /SAVE TRANSACTION \S+/
-          end
-
-          queries = grouped_queries.flatten
+          queries = include_release_savepoint_placeholder_queries(queries)
           # End of monkey-patch
 
           if count
@@ -37,6 +20,29 @@ module ARTest
           end
           result
         end
+      end
+
+      private
+
+      # Rails tests expect a save-point to be created and released. SQL Server does not release
+      # save-points and so the number of queries will be off. This monkey patch adds a placeholder queries
+      # to replace the missing save-point releases.
+      def include_release_savepoint_placeholder_queries(queries)
+        grouped_queries = [[]]
+
+        queries.each do |query|
+          if query =~ /SAVE TRANSACTION \S+/
+            grouped_queries << [query]
+          else
+            grouped_queries.last << query
+          end
+        end
+
+        grouped_queries.each do |group|
+          group.append "/* release savepoint placeholder for testing */" if group.first =~ /SAVE TRANSACTION \S+/
+        end
+
+        grouped_queries.flatten
       end
     end
   end
