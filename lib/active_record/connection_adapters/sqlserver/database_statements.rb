2a60a217750cdb2bf9d0d77651eae7572ec331ea
Aidan Haran <aidanharan@yahoo.com>
2024-09-02 18:14:20 +0100

    Fix serialized encrypted attributes (#1218)

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -321,10 +321,13 @@ module ActiveRecord
 
         def sp_executesql_sql_type(attr)
           if attr.respond_to?(:type)
-            return attr.type.sqlserver_type if attr.type.respond_to?(:sqlserver_type)
+            type = attr.type.is_a?(ActiveRecord::Normalization::NormalizedValueType) ? attr.type.cast_type : attr.type
+            type = type.subtype if type.serialized?
 
-            if attr.type.is_a?(ActiveRecord::Encryption::EncryptedAttributeType) && attr.type.instance_variable_get(:@cast_type).respond_to?(:sqlserver_type)
-              return attr.type.instance_variable_get(:@cast_type).sqlserver_type
+            return type.sqlserver_type if type.respond_to?(:sqlserver_type)
+
+            if type.is_a?(ActiveRecord::Encryption::EncryptedAttributeType) && type.instance_variable_get(:@cast_type).respond_to?(:sqlserver_type)
+              return type.instance_variable_get(:@cast_type).sqlserver_type
             end
           end
 
