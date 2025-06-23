d1ed46dd19d7398d7094dfd04afbb6f577a83d5b
B N <bnikitakis@gmail.com>
2025-06-23 15:00:26 +0300

    Added extra change_column with index test case

--- test/cases/change_column_index_test_sqlserver.rb
+++ test/cases/change_column_index_test_sqlserver.rb
@@ -16,18 +16,46 @@ class ChangeColumnIndexTestSqlServer < ActiveRecord::TestCase
     end
   end
 
+  class CreateBlogPostsWithMultipleIndexesOnTheSameColumn < ActiveRecord::Migration[8.0]
+    def up
+      create_table :blog_posts do |t|
+        t.string :title, limit: 15
+        t.string :subtitle
+      end
+      add_index :blog_posts, :title, unique: true, where: "([blog_posts].[title] IS NOT NULL)", name: "custom_index_name"
+      add_index :blog_posts, [:title, :subtitle], unique: true
+    end
+
+    def down
+      drop_table :blog_posts
+    end
+  end
+
   class ChangeClientsNameLength < ActiveRecord::Migration[8.0]
     def up
       change_column :clients, :name, :string, limit: 30
     end
   end
 
+  class ChangeBlogPostsTitleLength < ActiveRecord::Migration[8.0]
+    def up
+      change_column :blog_posts, :title, :string, limit: 30
+    end
+  end
+
   before do
+    @old_verbose = ActiveRecord::Migration.verbose
+    ActiveRecord::Migration.verbose = false
+
     CreateClientsWithUniqueIndex.new.up
+    CreateBlogPostsWithMultipleIndexesOnTheSameColumn.new.up
   end
 
   after do
     CreateClientsWithUniqueIndex.new.down
+    CreateBlogPostsWithMultipleIndexesOnTheSameColumn.new.down
+
+    ActiveRecord::Migration.verbose = @old_verbose
   end
 
   def test_index_uniqueness_is_maintained_after_column_change
@@ -46,5 +74,37 @@ class ChangeColumnIndexTestSqlServer < ActiveRecord::TestCase
     assert_equal indexes.size, 1
     assert_equal indexes.first.name, "index_clients_on_name"
     assert indexes.first.unique
+  end
+
+  def test_multiple_index_options_are_maintained_after_column_change
+    indexes = ActiveRecord::Base.connection.indexes("blog_posts")
+    columns = ActiveRecord::Base.connection.columns("blog_posts")
+    assert_equal columns.find { |column| column.name == "title" }.limit, 15
+    assert_equal indexes.size, 2
+
+    index_1 = indexes.find { |index| index.columns == ["title"] }
+    assert_equal index_1.name, "custom_index_name"
+    assert_equal index_1.where, "([blog_posts].[title] IS NOT NULL)"
+    assert index_1.unique
+
+    index_2 = indexes.find { |index| index.columns == ["title", "subtitle"] }
+    assert index_2.unique
+
+
+    ChangeBlogPostsTitleLength.new.up
+
+
+    indexes = ActiveRecord::Base.connection.indexes("blog_posts")
+    columns = ActiveRecord::Base.connection.columns("blog_posts")
+    assert_equal columns.find { |column| column.name == "title" }.limit, 30
+    assert_equal indexes.size, 2
+
+    index_1 = indexes.find { |index| index.columns == ["title"] }
+    assert_equal index_1.name, "custom_index_name"
+    assert_equal index_1.where, "([blog_posts].[title] IS NOT NULL)"
+    assert index_1.unique
+
+    index_2 = indexes.find { |index| index.columns == ["title", "subtitle"] }
+    assert index_2.unique
   end
 end
