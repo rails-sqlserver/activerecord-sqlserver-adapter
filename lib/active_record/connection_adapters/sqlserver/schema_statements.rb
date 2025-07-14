bc72b1d2378fb6c0815ebf402cbf8291ef38332b
Aidan Haran <aidanharan@yahoo.com>
2025-07-14 11:03:37 +0100

    Update error message
    Ref: https://github.com/rails/rails/pull/55313

--- lib/active_record/connection_adapters/sqlserver/schema_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/schema_statements.rb
@@ -241,7 +241,7 @@ module ActiveRecord
         end
 
         def rename_index(table_name, old_name, new_name)
-          raise ArgumentError, "Index name '#{new_name}' on table '#{table_name}' is too long; the limit is #{index_name_length} characters" if new_name.length > index_name_length
+          raise ArgumentError, "Index name '#{new_name}' on table '#{table_name}' is too long (#{new_name.length} characters); the limit is #{index_name_length} characters" if new_name.length > index_name_length
 
           identifier = SQLServer::Utils.extract_identifiers("#{table_name}.#{old_name}")
           execute_procedure :sp_rename, identifier.quoted, new_name, "INDEX"
