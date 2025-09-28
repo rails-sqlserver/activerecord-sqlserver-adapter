8217d47c61896977ee015253b52a947afdef7354
Aidan Haran <aidanharan@yahoo.com>
2025-09-28 11:33:39 +0100

    Modify namespace of ActiveRecord explain class
    Ref: https://github.com/rails/rails/pull/55736

--- lib/active_record/connection_adapters/sqlserver/core_ext/explain_subscriber.rb
+++ lib/active_record/connection_adapters/sqlserver/core_ext/explain_subscriber.rb
@@ -5,6 +5,6 @@ require "active_support/core_ext/kernel/reporting"
 ActiveSupport.on_load(:active_record) do
   silence_warnings do
     # Already defined in Rails
-    ActiveRecord::ExplainSubscriber::EXPLAINED_SQLS = /(select|update|delete|insert)\b/i
+    ActiveRecord::ExplainRegistry::Subscriber::EXPLAINED_SQLS = /(select|update|delete|insert)\b/i
   end
 end
