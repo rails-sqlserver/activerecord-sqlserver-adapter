49c8e006e2716299e8c3fe3909ec15688bab19b1
Aidan Haran <aidanharan@yahoo.com>
2026-08-06 19:13:26 +0100

    Refactor insert returning: handling to take QueryIntent directly

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -361,9 +361,10 @@ module ActiveRecord
 
         protected
 
+        # TODO: convert to "def apply_returning_to!(intent, returning)".
         def sql_for_insert(sql, pk, binds, returning)
           if pk.nil?
-            table_name = query_requires_identity_insert?(sql)
+            table_name = query_requires_identity_insert?(sql) || get_table_name(sql)
             pk = schema_cache.primary_keys(table_name)
           end
 
